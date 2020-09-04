218b712c5e9d63990fd754db674c1643c52fdf8f
Shiratama1529 <65014220+Shiratama1529@users.noreply.github.com>
2020-09-05 08:27:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let wins = 0;
+
+$(function() {
+    let myHand, comHand;
+    let myTemp, comTemp;
+
+    $('#button-0').click(function() {
+        Judge(0);
+    });
+
+    $('#button-1').click(function() {
+        Judge(1);
+    });
+
+    $('#button-2').click(function() {
+        Judge(2);
+    });
+
+    $('#button-restart').click(function() {
+        myHand = null;
+        comHand = null;
+        myTemp = null;
+        comTemp = null;
+        $('#end-preview').css('display','none');
+        $('#button-list').css('display','block');
+        $('#button-restart').css('display','none');
+        $('#label').html('突然ですが、じゃんけんを開始します。<br>出す手をクリックしてください。');
+    });
+});
+
+function Judge(myHand) {
+    comHand = Math.floor(Math.random() * Math.floor(3));
+    const $label = $('#label'), $shiratamaComment = $('#shiratama-comment');
+
+    switch(myHand) {
+        case 0:
+            myTemp = "グー";
+            break;
+        case 1:
+            myTemp = "チョキ";
+            break;
+        case 2:
+            myTemp = "パー";
+            break;
+    }
+
+    switch(comHand) {
+        case 0:
+            comTemp = "グー";
+            break;
+        case 1:
+            comTemp = "チョキ";
+            break;
+        case 2:
+            comTemp = "パー";
+            break;
+    }
+
+    if(myHand === 0 && comHand === 1 || myHand === 2 && comHand === 0 || myHand === 1 && comHand === 2) {
+        $label.html('<p>お前：' + myTemp + '　しらたま：' + comTemp + '<br/>あなたの勝ちです</p>');
+        $shiratamaComment.html('<p>強いね！！ ＾▽＾</p>');
+        wins++;
+    }
+    else if(myHand === comHand) {
+        $label.html('<p>お前：' + myTemp + '　しらたま：' + comTemp + '<br/>引き分けです</p>');
+        $shiratamaComment.html('<p>もう一回！ ＞_＜</p>');
+    }
+    else {
+        $label.html('<p>お前：' + myTemp + '　しらたま：' + comTemp + '<br/>あなたの負けです</p>');
+        $shiratamaComment.html('<p>ザッッコww ＾△＾</p>');
+        wins = 0;
+    }
+    
+    $('#end-preview').css('display','block');
+    $('#button-list').css('display','none');
+    $('#button-restart').css('display','block');
+    $('#wins-letters').html(wins + '連勝');
+}
